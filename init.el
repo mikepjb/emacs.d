70d45fcb9fe9b6fcfd6e6c8c1933c263cc25f7d2
Mike Bruce <mike@hypalynx.com>
2025-06-11 20:42:16 +0100

    trim down to 286 lines

--- init.el
+++ init.el
@@ -1,6 +1,4 @@
-;; Spartan Emacs Configuration, never more than 150 lines, sometimes less. -----
-;; Dynamic prose centering (~15 lines) - fixed margins would do
-;; still need a good way to go up/down paragraph on ZSA voyager, M-{ & M-} are not accessible
+;; -- Spartan Emacs Configuration, never more than 150 lines, sometimes less. --
 
 (setq inhibit-startup-screen t
       ring-bell-function 'ignore
@@ -9,7 +7,6 @@
       create-lockfiles nil
       use-short-answers t
       vc-follow-symlinks t
-      find-file-visit-truename t
       split-height-threshold 80
       split-width-threshold 160
       sql-input-ring-file-name (concat user-emacs-directory "sql-history")
@@ -23,35 +20,22 @@
       display-buffer-alist '(("\\*vc-dir\\*" display-buffer-pop-up-window)))
 
 (setq-default display-fill-column-indicator-column 80
-	      default-frame-width 160
-	      default-frame-height 60
 	      truncate-lines t) ;; no word wrap thanks
 
-(add-hook 'compilation-filter-hook 'comint-truncate-buffer)
-(add-hook 'inferior-lisp-mode-hook (lambda () (font-lock-mode -1)))
+(dolist (mode '(fido-vertical-mode global-auto-revert-mode show-paren-mode
+	   save-place-mode electric-pair-mode savehist-mode))
+  (funcall mode 1)) ;; enable these
 
-(dolist (base-mode
-	 '(fido-vertical-mode
-	   global-auto-revert-mode
-	   show-paren-mode
-	   save-place-mode
-	   electric-pair-mode
-	   savehist-mode))
-  (funcall base-mode 1))
+(load custom-file t)
+(load (concat user-emacs-directory "local.el") t)
 
-(when (executable-find "rg")
-  (setq grep-program "rg"
-	grep-command
-	"rg --color=never --no-heading --line-number --max-filesize=300K "))
-
-;; Bindings --------------------------------------------------------------------
+;; -- Bindings -----------------------------------------------------------------
 (defun +rg (pattern)
   (interactive "sSearch: ")
-  (let* ((git-root (or (vc-git-root default-directory)
-                       default-directory))
-         (default-directory git-root)
+  (let* ((default-directory (or (vc-git-root default-directory)
+				default-directory))
          (command (format "rg --color=always --smart-case --no-heading --line-number --column %s ."
-                         (shell-quote-argument pattern))))
+                          (shell-quote-argument pattern))))
     (grep command)))
 
 (defun +kill-region-or-backward-word ()
@@ -65,7 +49,6 @@
           (t (backward-kill-word 1)))))
 
 (defun +minibuffer-kill-backward ()
-  "Kill backward word, or go up directory if looks like file path."
   (interactive)
   (if (and (fboundp 'icomplete-fido-backward-updir)
            (string-match-p "/" (minibuffer-contents)))
@@ -94,23 +77,23 @@
 
 (defun +link-tags ()
   (when-let ((git-dir (locate-dominating-file default-directory ".git")))
-    (setq-local tags-table-list 
-                (mapcar (lambda (f) (expand-file-name f git-dir))
-                        '(".git/tags/project" ".git/tags/deps" ".git/tags/external")))))
+    (setq-local
+     tags-table-list 
+     (mapcar (lambda (f) (expand-file-name f git-dir))
+             '(".git/tags/project" ".git/tags/deps" ".git/tags/external")))))
 
 (add-hook 'find-file-hook #'+link-tags)
 
 (defun +compile ()
   "Compile from directory with build file."
   (interactive)
-  (let ((build-dir
+  (let ((default-directory
 	 (locate-dominating-file
 	  default-directory
 	  (lambda (dir)
 	    (seq-some (lambda (f) (file-exists-p (expand-file-name f dir)))
 		      '("Makefile" "go.mod" "package.json"))))))
-    (let ((default-directory (or build-dir default-directory)))
-      (call-interactively 'compile))))
+    (call-interactively 'compile)))
 
 (defmacro ff (&rest path)
   `(lambda ()
@@ -131,7 +114,7 @@
 		   ("C-h" delete-backward-char)
 		   ("C-j" newline) ;; because electric-indent overrides this
 		   ("C-w" +kill-region-or-backward-word)
-		   ("M-C" org-agenda-list) ;; a.k.a checklist
+		   ("C-;" completion-at-point)
 		   ("M-E" emoji-search)	   ;; express yourself!
 		   ("M-F" toggle-frame-fullscreen)
 		   ("M-I" +rg) ;; I for investigate
@@ -147,10 +130,10 @@
 (global-set-key (kbd "M-H") help-map)
 (global-set-key (kbd "M-S") search-map)
 
-(with-eval-after-load 'vc-dir
+(with-eval-after-load 'vc-dir ;; why isn't this a default?
   (define-key vc-dir-mode-map (kbd "R") 'vc-revert))
 
-;; Editing setup ---------------------------------------------------------------
+;; -- Editing setup ------------------------------------------------------------
 (dolist (hook '(prog-mode-hook css-mode-hook))
   (add-hook hook (lambda ()
 		   (display-line-numbers-mode 1)
@@ -161,7 +144,6 @@
 		   (local-set-key (kbd "M-p") 'backward-paragraph))))
 
 (define-derived-mode templ-mode prog-mode "Templ"
-  "Major mode for editing templ files."
   (add-hook 'after-save-hook 'format-buffer-templ nil t))
 
 (add-to-list 'auto-mode-alist '("\\.templ\\'" . templ-mode))
@@ -172,18 +154,14 @@
     (shell-command (concat "templ fmt " (shell-quote-argument (buffer-file-name))))
     (revert-buffer t t t)))
 
-(define-key isearch-mode-map (kbd "C-j") #'+isearch-exit-other-end)
-(defun +isearch-exit-other-end ()
-  "Exit isearch, at the opposite end of the string."
-  (interactive)
-  (isearch-exit) (goto-char isearch-other-end))
-
-(defun should-center-buffer-p ()
-  (memq major-mode '(org-mode markdown-mode)))
+(define-key isearch-mode-map (kbd "C-j")
+	    (lambda ()
+	      (interactive)
+	      (isearch-exit) (goto-char isearch-other-end)))
 
 (defun center-prose-buffer-margins ()
   (set-window-margins nil 0 0)
-  (when (should-center-buffer-p)
+  (when (memq major-mode '(org-mode markdown-mode))
     (let* ((char-width-pix (frame-char-width))
            (window-width-pix (window-body-width nil t))  ; t = pixels!
            (target-width-chars 80)
@@ -205,18 +183,16 @@
 (add-hook 'org-mode-hook (lambda () (prose-config) (org-indent-mode)))
 
 (advice-add 'org-refile :after 'org-save-all-org-buffers)
-(with-eval-after-load 'org
-  (define-key org-mode-map (kbd "M-i") 'org-todo))
 
 (with-eval-after-load 'sql
   (define-key sql-mode-map (kbd "C-c f") 
     (lambda () (interactive)
       (shell-command-on-region (point-min) (point-max) "pg_format -" nil t))))
 
-;; Appearance ------------------------------------------------------------------
-(dolist (ui-mode ;; disable these
+;; -- Appearance ---------------------------------------------------------------
+(dolist (ui-mode
 	 '(menu-bar-mode tool-bar-mode blink-cursor-mode))
-  (funcall ui-mode -1))
+  (funcall ui-mode -1)) ;; disable these
 
 (when window-system
   (scroll-bar-mode -1)
@@ -228,36 +204,60 @@
   (set-face-attribute 'default nil :font *default-font* :height 160)
   (set-face-attribute 'variable-pitch nil :font *writing-font* :height 160)
 
-  ;; breaks when loading org-mode.. find after loading
   (with-eval-after-load 'org
     (dolist (group '(org-block org-code org-verbatim org-table))
-      (set-face-attribute group nil :font *default-font*)))
-  )
+      (set-face-attribute group nil :font *default-font*))))
 
 (setq-default cursor-type 'box)
 
-(dolist (attr `((alpha (95 . 95))
-		(width 160)
-		(height 60)))
+(dolist (attr `((alpha (95 . 95)) (width 100) (height 60)))
   (set-frame-parameter (selected-frame) (car attr) (cadr attr)))
-
-(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
 
 (ignore-errors (load-theme 'flow t))
 
-;; Built-in Language Assignment ------------------------------------------------
-(add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . conf-mode))
-(add-to-list 'auto-mode-alist '("\\.\\(json\\|ts\\|tsx\\)\\'" . js-mode))
-
-;; Packages (mostly just language major-modes) ---------------------------------
+;; -- Languages ----------------------------------------------------------------
 (add-to-list 'load-path "~/.emacs.d/external-modes/")
 
 ;; Autoload the modes (only loads when actually used)
 (autoload 'clojure-mode "clojure-mode" "Major mode for Clojure" t)
 (autoload 'go-mode "go-mode" "Major mode for Go" t)
 (autoload 'markdown-mode "markdown-mode" "Major mode for Markdown" t)
-(autoload 'paredit-mode "paredit" "Minor mode for balanced parentheses" t)
 
+;; Configure only when modes are actually loaded
+(with-eval-after-load 'clojure-mode
+  (add-hook 'clojure-mode-hook 'subword-mode)
+  (add-hook 'clojure-mode-hook
+	    '(lambda ()
+	       (define-key clojure-mode-map (kbd "C-x C-e")
+			   (lambda ()
+			     (interactive)
+			     (if (region-active-p)
+				 (call-interactively #'lisp-eval-region)
+			       (lisp-eval-last-sexp))))
+	       (define-key clojure-mode-map (kbd "C-c C-k")
+			   (lambda ()
+			     (interactive)
+			     (save-excursion
+			       (mark-whole-buffer)
+			       (call-interactively #'lisp-eval-region)))))))
+
+(with-eval-after-load 'go-mode
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq-default gofmt-command "goimports"
+                gofmt-show-errors 'echo))
+
+(with-eval-after-load 'markdown-mode
+  (add-hook 'markdown-mode-hook 'prose-config))
+
+;; Auto-mode associations (triggers autoload when opening files)
+(add-to-list 'auto-mode-alist '("\\.\\(clj\\|cljs\\|cljc\\|edn\\)\\'"
+				. clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(json\\|ts\\|tsx\\)\\'" . js-mode))
+
+;; -- Paredit ------------------------------------------------------------------
 (defun +paredit-RET ()
   (interactive)
   (call-interactively (pcase major-mode
@@ -284,39 +284,3 @@
   (define-key paredit-mode-map (kbd "M-r") #'+paredit-M-r)
   (define-key paredit-mode-map (kbd "M-k") 'paredit-forward-barf-sexp)
   (define-key paredit-mode-map (kbd "M-l") 'paredit-forward-slurp-sexp))
-
-;; Auto-mode associations (triggers autoload when opening files)
-(add-to-list 'auto-mode-alist '("\\.\\(clj\\|cljs\\|cljc\\|edn\\)\\'"
-				. clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-;; Configure only when modes are actually loaded
-(with-eval-after-load 'clojure-mode
-  (add-hook 'clojure-mode-hook 'subword-mode)
-  (add-hook 'clojure-mode-hook
-	    '(lambda ()
-	      (define-key clojure-mode-map (kbd "C-x C-e")
-			  (lambda ()
-			    (interactive)
-			    (if (region-active-p)
-				(call-interactively #'lisp-eval-region)
-			      (lisp-eval-last-sexp))))
-	      (define-key clojure-mode-map (kbd "C-c C-k")
-			  (lambda ()
-			    (interactive)
-			    (save-excursion
-			      (mark-whole-buffer)
-			      (call-interactively #'lisp-eval-region)))))))
-
-(with-eval-after-load 'go-mode
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (setq-default gofmt-command "goimports"
-                gofmt-show-errors 'echo))
-
-(with-eval-after-load 'markdown-mode
-  (add-hook 'markdown-mode-hook 'prose-config))
-
-;; Local files -----------------------------------------------------------------
-(load custom-file t)
-(load (concat user-emacs-directory "local.el") t)
