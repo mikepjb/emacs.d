8ed2080cdd432ea44afb5cda094fca17079fe911
Mike Bruce <mike@hypalynx.com>
2026-04-01 23:33:47 +0100

    minor init.el tweaks

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; 1. need to organise the file..
+;; 2. REPLs.. local ai etc
+
 ;; Behaviour
 (setq search-wrap-around t)
 (setq ring-bell-function 'ignore)
@@ -36,7 +39,7 @@
     (up-list)
     (let ((close (char-before)))
       (delete-char -1)
-      (forward-sexp)
+      (forward-sexp) ;; TODO make sure that sexp include []{} for clojure!
       (insert close))))
 
 (defun forward-barf ()
@@ -54,24 +57,24 @@
 
 (dolist (hook '(prog-mode-hook css-mode-hook))
   (add-hook hook (lambda ()
-           (display-line-numbers-mode 1) (column-number-mode 1)
-           (auto-revert-mode 1)
-           (display-fill-column-indicator-mode 1) (hl-line-mode 1))))
+	   (display-line-numbers-mode 1) (column-number-mode 1)
+	   (auto-revert-mode 1)
+	   (display-fill-column-indicator-mode 1) (hl-line-mode 1))))
 
 (defun center-prose (buf)
   (when (buffer-live-p buf)
     (dolist (win (get-buffer-window-list buf nil 'visible))
       (let ((margin (max 0 (round (/ (- (window-body-width win t)
-                                        (* 80 (frame-char-width)))
-                                     2.0 (frame-char-width))))))
-        (set-window-margins win margin margin)))))
+					(* 80 (frame-char-width)))
+				     2.0 (frame-char-width))))))
+	(set-window-margins win margin margin)))))
 
 (defun prose-config ()
   (visual-line-mode 1)
   (let ((buf (current-buffer)))
     (add-hook 'window-size-change-functions
-              (lambda (&rest _) (center-prose buf))
-              nil t)))
+	      (lambda (&rest _) (center-prose buf))
+	      nil t)))
 
 (add-hook 'org-mode-hook (lambda () (prose-config) (org-indent-mode)))
 
@@ -80,7 +83,7 @@
 (defun +kill-region-or-backward-word ()
   (interactive)
   (cond ((region-active-p) (call-interactively #'kill-region))
-        (t (backward-kill-word 1))))
+	(t (backward-kill-word 1))))
 
 (defun +minibuffer-C-w ()
   (interactive)
@@ -91,11 +94,19 @@
   (define-key icomplete-minibuffer-map (kbd "C-w") #'+minibuffer-C-w)
   (define-key icomplete-minibuffer-map (kbd "C-e") #'icomplete-ret))
 
+(defun +local-ai ()
+  (interactive)
+  (start-process "llama-server" "*llama-server*"
+		 "llama-server" "--port" "7777"
+		 "--model" "~/models/Qwen3.5-9B-UD-Q3_K_XL.gguf"))
+
 (defmacro il (&rest body) `(lambda () (interactive) ,@body))
+(defmacro ff (&rest path) `(il (find-file (concat ,@path))))
 
 ;; Bindings
 (dolist (binding `(("M-o" other-window) ("M-O" delete-other-windows)
 		   ("M-s" save-buffer)
+		   ("C-c P" ,(ff "~/src"))
 		   ("C-c g" vc-dir-root)
 		   ("C-w" +kill-region-or-backward-word)
 		   ("M-k" forward-barf) ("M-l" forward-slurp)
