2f3f71e8c6a07190ede67d161adf35f87585107a
Mike Bruce <mike@hypalynx.com>
2026-04-02 23:43:14 +0100

    init.el refinement

--- init.el
+++ init.el
@@ -1,98 +1,33 @@
-;; -- Spartan Emacs configuration ------------------ -*- lexical-binding: t; -*-
-;;; repl + repl lookup (rebind man bind?)
-;; PATH, hardcode.. or defer bash via process?
+;;; package --- init -------- -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;; A Spartan Emacs configuration.
+;;;
+;;; This is designed to be a minimal environment that is stable, to
+;;; help the user get things done!
+
+;;; Code:
+
+;;; Configuration:
 
 (defconst *project-identifiers*
   '("Makefile" "gradlew" "pom.xml" "go.mod" "package.json" "deps.edn" ".git"))
 
-(defun +compile ()
-  (interactive)
-  (+with-context (call-interactively 'compile)))
+(defconst *repl-table*
+  '((python run-python)
+    (ruby irb)))
 
-(defmacro il (&rest body) `(lambda () (interactive) ,@body))
-(defmacro ff (&rest path) `(il (find-file (concat ,@path))))
-
-;;; Input
 (pcase system-type
   ('darwin (setq mac-command-modifier 'meta))
   ('gnu/linux (setq x-super-keysym 'meta)))
 
-;; TODO can we/should be have all bindings (incl. ones from other languages) in one place? How does this work if the package isn't available?
-(defconst *bindings*
-  `(("M-o" other-window) ("M-O" delete-other-windows)
-    ("M-s" save-buffer)
-    ("M-C" ,(il (comint-run "navi")))
-    ("C-c C-l" flycheck-list-errors)
-    ("C-c i" ,(ff user-emacs-directory "init.el"))
-    ("C-c n" ,(ff user-emacs-directory "notes/index.org"))
-    ("C-c p" project-find-files)
-    ("C-c P" ,(ff "~/src"))
-    ("C-c g" vc-dir-root)
-    ("C-c C-g" vc-print-root-log)
-    ("C-c k" split-window-below)
-    ("C-c l" split-window-right)
-    ("M-T" eshell)
-    ("M-/" replace-string)
-    ("M-n" forward-paragraph) ;; more ergo than M-{} but not that easy
-    ("M-p" backward-paragraph)
-    ("C-w" +kill-region-or-backward-word)
-    ("M-i" rgrep)
-    ("M-K" kill-whole-line)
-    ("M-k" +lisp-forward-barf)
-    ("M-l" +lisp-forward-slurp)
-    ("C-c s" +lisp-splice-sexp)
-    ("C-h" delete-backward-char) ("C-j" newline) ;; autoindents
-    ("M-j" ,(il (join-line -1)))
-    ("M-RET" toggle-frame-fullscreen)
-    ("M-H" ,help-map)
-    ("M-D" duplicate-line)
-    ("C-c m" recompile)  ("C-c M" +compile)
-    ("C-;" dabbrev-expand)
-    ("C-c C-s" ,search-map)))
-
-(dolist (bind *bindings*)
-  (global-set-key (kbd (car bind)) (cadr bind)))
-
-(with-eval-after-load 'icomplete
-  (define-key icomplete-minibuffer-map (kbd "C-w") #'+minibuffer-C-w)
-  (define-key icomplete-minibuffer-map (kbd "C-e") #'icomplete-ret))
-
-;;; General Settings
-(setq search-wrap-around t ;; does this need to be setq-default?
-      isearch-wrap-pause 'no
+(setq isearch-wrap-pause 'no
       ring-bell-function 'ignore
       create-lockfiles nil
       use-short-answers t
       frame-resize-pixelwise t
-      org-hide-emphasis-markers t
-      org-export-with-section-numbers nil ;; essential for exporting
       backup-directory-alist `(("." . ,(concat user-emacs-directory "saves")))
-      large-file-warning-threshold (* 512 1024 1024) ; 500MB
-      )
-
-(defconst *general-modes*
-  '(fido-vertical-mode
-    global-auto-revert-mode
-    save-place-mode
-    savehist-mode))
-
-(dolist (mode *general-modes*) (funcall mode 1))
-
-;;; Editor Settings
-(setq-default truncate-lines t
-	      indent-tabs-mode nil tab-width 2 standard-indent 2
-	      display-fill-column-indicator-column 80)
-
-(setq whitespace-style '(face trailing tabs empty indentation::space)
-      )
-
-(defconst *editing-modes*
-  '(show-paren-mode
-    electric-pair-mode))
-
-(dolist (mode *editing-modes*) (funcall mode 1))
-
-;;; unorganised
+      large-file-warning-threshold (* 512 1024 1024)) ; 500MB
 
 (setq custom-file (concat user-emacs-directory "local.el"))
 (load custom-file t)
@@ -105,80 +40,86 @@
   (scroll-bar-mode -1)
   (fringe-mode -1)
 
-  (defconst *font* (seq-find #'x-list-fonts '("Rec Mono Casual" "Monospace")))
-  (set-face-attribute 'default nil :font *font* :height 160))
+  (set-face-attribute
+   'default nil
+   :font (seq-find #'x-list-fonts '("Rec Mono Casual" "Monospace"))
+   :height 160))
 
-(defun +lisp-forward-slurp ()
+(setq-default mode-line-format
+  '(" "
+    (:eval (propertize (+flow-truncate-buffer-name)
+           'face '(:foreground "#00ccff" :weight bold)))
+    " [%*]"
+    mode-line-format-right-align
+    (:eval (when (and (boundp 'vc-mode) vc-mode)
+       (concat (propertize " | " 'face '(:foreground "#787878"))
+         (replace-regexp-in-string "^ Git[-:]" "" vc-mode))))
+    (:eval (propertize " | " 'face '(:foreground "#787878")))
+    "%l:%c"
+    (:eval (propertize " | " 'face '(:foreground "#787878")))
+    (:eval (when-let ((proc (get-buffer-process (current-buffer))))
+       (propertize (format "[%s] " (process-name proc))
+       'face '(:foreground "#ff00ff" :weight bold))))
+    (:eval (replace-regexp-in-string "-mode$" "" (symbol-name major-mode)))
+    " "))
+n
+(defconst *general-modes*
+  '(fido-vertical-mode
+    global-auto-revert-mode
+    save-place-mode
+    savehist-mode))
+
+(dolist (mode *general-modes*) (funcall mode 1))
+
+;;; Editor Settings:
+
+(setq-default truncate-lines t
+              indent-tabs-mode nil tab-width 2 standard-indent 2
+              display-fill-column-indicator-column 80
+              whitespace-style '(face trailing tabs empty indentation::space))
+
+(defconst *editing-modes*
+  '(show-paren-mode
+    electric-pair-mode))
+
+(dolist (mode *editing-modes*) (funcall mode 1))
+
+;;; Functions:
+
+(defmacro +with-context (&rest body)
+  `(let ((default-directory
+          (or (cl-some (lambda (f) (locate-dominating-file default-directory f))
+                       *project-identifiers*)
+              default-directory)))
+     ,@body))
+
+(defun +compile ()
   (interactive)
-  (save-excursion
-    (up-list)
-    (let ((close (char-before)))
-      (delete-char -1)
-      (forward-sexp)
-      (insert close))))
+  (+with-context (call-interactively 'compile)))
 
-(defun +lisp-forward-barf ()
-  (interactive)
-  (save-excursion
-    (up-list)
-    (let ((close (char-before)))
-      (delete-char -1)
-      (backward-sexp)
-      (delete-horizontal-space)
-      (insert (string close ?\s)))))
-
-(defun +lisp-splice-sexp ()
-  (interactive)
-  (save-excursion
-    (backward-up-list)
-    (delete-char 1)
-    (forward-sexp)
-    (delete-char -1)))
-
-(use-package clojure-mode :ensure t)
-(use-package json-mode :ensure t)
-(use-package project :ensure nil)
-(use-package org :ensure nil
-  :custom
-  (org-hide-emphasis-markers t)
-  (org-export-with-section-numbers nil))
-
-(use-package flycheck
-  :ensure nil
-  :config (global-flycheck-mode 1))
-
-(when (require 'ansi-color nil)
-  (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter))
-
-(dolist (hook '(prog-mode-hook css-mode-hook))
-  (add-hook hook (lambda ()
-	   (display-line-numbers-mode 1) (column-number-mode 1)
-	   (auto-revert-mode 1)
-	   (display-fill-column-indicator-mode 1) (hl-line-mode 1))))
+(defmacro il (&rest body) `(lambda () (interactive) ,@body))
+(defmacro ff (&rest path) `(il (find-file (concat ,@path))))
 
 (defun center-prose (buf)
   (when (buffer-live-p buf)
     (dolist (win (get-buffer-window-list buf nil 'visible))
       (let ((margin (max 0 (round (/ (- (window-body-width win t)
-					(* 80 (frame-char-width)))
-				     2.0 (frame-char-width))))))
-	(set-window-margins win margin margin)))))
+          (* 80 (frame-char-width)))
+             2.0 (frame-char-width))))))
+  (set-window-margins win margin margin)))))
 
 (defun prose-config ()
   (visual-line-mode 1)
   (let ((buf (current-buffer)))
     (add-hook 'window-size-change-functions
-	      (lambda (&rest _) (center-prose buf))
-	      nil t)))
-
-(add-hook 'org-mode-hook (lambda () (prose-config) (org-indent-mode)))
-
-(add-hook 'before-save-hook 'whitespace-cleanup)
+        (lambda (&rest _) (center-prose buf))
+        nil t)))
 
 (defun +kill-region-or-backward-word ()
   (interactive)
   (cond ((region-active-p) (call-interactively #'kill-region))
-	(t (backward-kill-word 1))))
+  ((bound-and-true-p paredit-mode) (paredit-backward-kill-word))
+  (t (backward-kill-word 1))))
 
 (defun +minibuffer-C-w ()
   (interactive)
@@ -191,74 +132,164 @@
    "llama-server" "*llama-server*"
    "llama-server" "--port" "7777"
    "--model" (expand-file-name
-	      "~/models/Qwen3.5-9B-UD-Q3_K_XL.gguf")))
-
-(load-theme 'modus-vivendi t)
-
-;;; Modeline
-(defun +flow-truncate-buffer-name ()
-  "Show right 1/3 of buffer name, prepend '<' if truncated."
-  (let* ((name (if buffer-file-name
-		   (abbreviate-file-name buffer-file-name)
-		 (buffer-name)))
-	 (max-len (/ (window-width) 3)))
-    (if (<= (length name) max-len)
-	name
-      (concat "<" (substring name (- (length name) (- max-len 1)))))))
-
-(setq-default mode-line-format
-  '(" "
-    (:eval (propertize (+flow-truncate-buffer-name)
-		       'face '(:foreground "#00ccff" :weight bold)))
-    " [%*]"
-    mode-line-format-right-align
-    (:eval (when (and (boundp 'vc-mode) vc-mode)
-	     (concat (propertize " | " 'face '(:foreground "#787878"))
-		     (replace-regexp-in-string "^ Git[-:]" "" vc-mode))))
-    (:eval (propertize " | " 'face '(:foreground "#787878")))
-    "%l:%c"
-    (:eval (propertize " | " 'face '(:foreground "#787878")))
-    (:eval (when-let ((proc (get-buffer-process (current-buffer))))
-	     (propertize (format "[%s] " (process-name proc))
-			 'face '(:foreground "#ff00ff" :weight bold))))
-    (:eval (replace-regexp-in-string "-mode$" "" (symbol-name major-mode)))
-    " "))
-
-(defmacro +with-context (&rest body)
-  `(let ((default-directory
-	  (or (cl-some (lambda (f) (locate-dominating-file default-directory f))
-		       *project-identifiers*)
-	      default-directory)))
-     ,@body))
+        "~/models/Qwen3.5-9B-UD-Q3_K_XL.gguf")))
 
 (defun +project-tags-generate ()
   "Manually generate tags for current project."
   (interactive)
   (when-let ((project-root (cl-some (lambda (f) (locate-dominating-file default-directory f))
-				    *project-identifiers*)))
+            *project-identifiers*)))
     (let ((tags-file (expand-file-name (format "~/.emacs.d/.tag-store/%s.tags"
-					       (file-name-nondirectory (directory-file-name project-root))))))
+                 (file-name-nondirectory (directory-file-name project-root))))))
       (ignore-errors (make-directory (file-name-directory tags-file) t))
       (make-process
        :name "ctags"
        :buffer (get-buffer-create "*ctags*")
        :command `("ctags" "--output-format=etags" "-f" ,tags-file "-R"
-		  "--languages=all" "--exclude=node_modules" ,project-root)
+      "--languages=all" "--exclude=node_modules" ,project-root)
        :sentinel (lambda (_ event)
-		   (when (string-match-p "finished" event)
-		     (message "Tags generated at %s" tags-file))))
+       (when (string-match-p "finished" event)
+         (message "Tags generated at %s" tags-file))))
       (when (file-exists-p tags-file) ;; assign newly generated tags file
-	(setq-local tags-table-list (list tags-file))
-	(visit-tags-table tags-file t)))))
+  (setq-local tags-table-list (list tags-file))
+  (visit-tags-table tags-file t)))))
 
 (defun +project-tags-load ()
   "Load tags file for current project if it exists."
   (when-let ((project-root (cl-some (lambda (f) (locate-dominating-file default-directory f))
-				    *project-identifiers*)))
+            *project-identifiers*)))
     (let ((tags-file (expand-file-name (format "~/.emacs.d/.tag-store/%s.tags"
-					       (file-name-nondirectory (directory-file-name project-root))))))
+                 (file-name-nondirectory (directory-file-name project-root))))))
       (when (file-exists-p tags-file)
-	(setq-local tags-table-list (list tags-file))
-	(visit-tags-table tags-file t)))))
+  (setq-local tags-table-list (list tags-file))
+  (visit-tags-table tags-file t)))))
 
+(defun +flow-truncate-buffer-name ()
+  "Show right 1/3 of buffer name, prepend '<' if truncated."
+  (let* ((name (if buffer-file-name
+       (abbreviate-file-name buffer-file-name)
+     (buffer-name)))
+   (max-len (/ (window-width) 3)))
+    (if (<= (length name) max-len)
+  name
+      (concat "<" (substring name (- (length name) (- max-len 1)))))))
+
+(defun +paredit-RET ()
+  (interactive)
+  (call-interactively
+   (pcase major-mode
+     ('inferior-lisp-mode 'comint-send-input)
+     ('minibuffer-mode 'read--expression-try-read)
+     (_ 'paredit-RET))))
+
+(defun +paredit-M-r ()
+  (interactive)
+  (call-interactively
+   (pcase major-mode
+     ('inferior-lisp-mode 'comint-history-isearch-backward-regexp)
+     ('minibuffer-mode 'previous-matching-history-element)
+     (_ 'paredit-raise-sexp))))
+
+;; Input/Keybinds ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(dolist (bind
+         `(
+           ("M-s" save-buffer)
+           ("M-C" ,(il (comint-run "navi")))
+           ("C-c C-l" flycheck-list-errors)
+           ("C-c i" ,(ff user-emacs-directory "init.el"))
+           ("C-c n" ,(ff user-emacs-directory "notes/index.org"))
+           ("C-c p" project-find-files)
+           ("C-c P" ,(ff "~/src"))
+           ("M-RET" toggle-frame-fullscreen)
+           ("M-H" ,help-map)
+
+           ;; Split management
+           ("C-c k" ,(il (select-window (split-window-below))))
+           ("C-c l" ,(il (select-window (split-window-right))))
+           ("C-c o" delete-other-windows)
+           ("M-o" ,(il (other-window 1)))
+           ("M-O" ,(il (other-window -1)))
+
+           ;; Code Tools
+           ("M-E" flycheck-list-errors)
+           ("M-i" rgrep)
+           ("C-c g" vc-dir-root)
+           ("C-c C-g" vc-print-root-log)
+
+           ("M-T" eshell)
+
+           ;; Editing
+           ("M-/" replace-string)
+           ("M-n" forward-paragraph) ;; more ergo than M-{} but not that easy
+           ("M-p" backward-paragraph)
+           ("C-w" +kill-region-or-backward-word)
+           ("M-K" kill-whole-line)
+           ("C-h" delete-backward-char)
+           ("C-j" newline) ;; autoindents
+           ("M-j" ,(il (join-line -1)))
+
+           ("M-D" duplicate-line)
+
+           ("C-c m" recompile)  ("C-c M" +compile)
+           ("C-;" execute-extended-command)
+           ;; ("C-;" dabbrev-expand)
+           ("C-c C-s" ,search-map)))
+  (global-set-key (kbd (car bind)) (cadr bind)))
+
+(with-eval-after-load 'icomplete
+  (define-key icomplete-minibuffer-map (kbd "C-w") #'+minibuffer-C-w)
+  (define-key icomplete-minibuffer-map (kbd "C-e") #'icomplete-ret))
+
+;;; Packages:
+
+(use-package clojure-mode :ensure t)
+(use-package json-mode :ensure t)
+(use-package project :ensure nil)
+(use-package org :ensure nil
+  :custom
+  (org-ellipsis " ▼")
+  (org-hide-emphasis-markers t)
+  (org-export-with-section-numbers nil)
+  (org-hide-emphasis-markers t)
+  (org-export-with-section-numbers nil))
+
+(use-package paredit :ensure t
+  :hook ((clojure-mode-hook
+          emacs-lisp-mode-hook
+          inferior-lisp-mode-hook lisp-data-mode-hook
+          eval-expression-minibuffer-setup-hook)
+   . #'enable-paredit-mode)
+  :bind (:map paredit-mode-map
+        ("C-j" . +paredit-RET)
+        ("M-r" . +paredit-M-r)
+        ("M-k" . paredit-forward-barf-sexp)
+        ("M-l" . paredit-forward-slurp-sexp)
+        ("C-;" . paredit-splice-sexp)
+        ("M-s" . nil)))
+
+(use-package flycheck
+  :ensure nil
+  :config (global-flycheck-mode 1))
+
+(use-package gptel :ensure t)
+(use-package ansi-color :ensure nil
+  :hook (compilation-filter-hook ansi-color-compilation-filter))
+
+(dolist (hook '(prog-mode-hook css-mode-hook))
+  (add-hook hook (lambda ()
+     (display-line-numbers-mode 1) (column-number-mode 1)
+     (auto-revert-mode 1)
+     (display-fill-column-indicator-mode 1) (hl-line-mode 1))))
+
+(add-hook 'org-mode-hook (lambda () (prose-config) (org-indent-mode)))
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'find-file-hook #'+project-tags-load)
+
+(load-theme 'modus-vivendi t)
+
+;; (mapcar #'cdr *repl-table*)
+
+(provide 'init)
+;;; init.el ends here
