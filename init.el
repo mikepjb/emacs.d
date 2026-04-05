2234142aefe695ba73cc2cb7d220d75e6c847985
Mike Bruce <mike@hypalynx.com>
2026-04-05 07:58:53 +0100

    Add REPL helper + tidy

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; package --- init -------- -*- lexical-binding: t; -*-
+;;; Package --- init -------- -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 ;;; A Spartan Emacs configuration.
@@ -14,8 +14,11 @@
   '("Makefile" "gradlew" "pom.xml" "go.mod" "package.json" "deps.edn" ".git"))
 
 (defconst *repl-table*
-  '((python run-python)
-    (ruby irb))) ;; how to handle SQL? all .sql
+  '((python-mode      run-python)
+    (ruby-mode        comint-run "irb")
+    (emacs-lisp-mode  ielm)
+    (edn-mode         inferior-lisp "clojure")
+    (clojure-mode     inferior-lisp "clojure")))
 
 (dolist (meta-key '(mac-command-modifier x-super-keysym))
   (when (boundp meta-key)
@@ -30,7 +33,7 @@
       backup-directory-alist `(("." . ,(concat user-emacs-directory "saves")))
       large-file-warning-threshold (* 512 1024 1024) ; 500MB
       custom-file (concat user-emacs-directory "local.el")
-      auth-sources (concat user-emacs-directory ".authinfo.gpg")
+      auth-sources (list (concat user-emacs-directory ".authinfo.gpg"))
       package-archives (if-let ((mirror (getenv "EMACS_PACKAGE_MIRROR")))
                            `(("local" . ,mirror))
                          '(("melpa" . "https://melpa.org/packages/")
@@ -57,7 +60,7 @@
     mode-line-format-right-align
     (:eval (when (and (boundp 'vc-mode) vc-mode)
        (concat (propertize " | " 'face '(:foreground "#787878"))
-         (replace-regexp-in-string "^ Git[-:]" "" vc-mode))))
+         (replace-regexp-in-string " Git[-:]" "" vc-mode))))
     (:eval (propertize " | " 'face '(:foreground "#787878")))
     "%l:%c"
     (:eval (propertize " | " 'face '(:foreground "#787878")))
@@ -82,11 +85,7 @@
  display-fill-column-indicator-column 80
  whitespace-style '(face trailing tabs empty indentation::space))
 
-(defconst *editing-modes*
-  '(show-paren-mode
-    electric-pair-mode))
-
-(dolist (mode *editing-modes*) (funcall mode 1))
+(dolist (mode '(show-paren-mode electric-pair-mode)) (funcall mode 1))
 
 ;;; Functions:
 
@@ -104,26 +103,37 @@
 (defmacro il (&rest body) `(lambda () (interactive) ,@body))
 (defmacro ff (&rest path) `(il (find-file (concat ,@path))))
 
-;; (defun center-prose (buf)
-;;   (when (buffer-live-p buf)
-;;     (dolist (win (get-buffer-window-list buf nil 'visible))
-;;       (let ((margin (max 0 (round (/ (- (window-body-width win t)
-;;           (* 80 (frame-char-width)))
-;;              2.0 (frame-char-width))))))
-;;   (set-window-margins win margin margin)))))
+(defun +repl-cmd   (entry) (cadr entry))
+(defun +repl-args  (entry) (cddr entry))
+(defun +repl-label (entry)
+  (if (+repl-args entry)
+      (car (last (+repl-args entry)))
+    (symbol-name (+repl-cmd entry))))
 
-;; (defun prose-config ()
-;;   (visual-line-mode 1)
-;;   (let ((buf (current-buffer)))
-;;     (add-hook 'window-size-change-functions
-;;         (lambda (&rest _) (center-prose buf))
-;;         nil t)))
+(defun +repl-run (entry)
+  (if (+repl-args entry)
+      (apply (+repl-cmd entry) (+repl-args entry))
+    (call-interactively (+repl-cmd entry))))
+
+(defun +repl (arg)
+  (interactive "P")
+  (other-window-prefix)
+  (if arg
+      (let* ((unique (seq-uniq *repl-table*
+                               (lambda (a b) (equal (+repl-label a) (+repl-label b)))))
+             (choice (completing-read "REPL: " (mapcar #'+repl-label unique) nil t))
+             (entry  (seq-find (lambda (e) (equal (+repl-label e) choice)) *repl-table*)))
+        (+repl-run entry))
+    (let ((entry (assq major-mode *repl-table*)))
+      (if entry
+          (+repl-run entry)
+        (user-error "No REPL configured for %s" major-mode)))))
 
 (defun +kill-region-or-backward-word ()
   (interactive)
   (cond ((region-active-p) (call-interactively #'kill-region))
-  ((bound-and-true-p paredit-mode) (paredit-backward-kill-word))
-  (t (backward-kill-word 1))))
+        ((bound-and-true-p paredit-mode) (paredit-backward-kill-word))
+        (t (backward-kill-word 1))))
 
 (defun +minibuffer-C-w ()
   (interactive)
@@ -131,43 +141,23 @@
       (icomplete-fido-backward-updir) (backward-kill-word 1)))
 
 (defun +project-tags-generate ()
-  "Manually generate tags for current project."
   (interactive)
-  (when-let ((project-root (cl-some (lambda (f) (locate-dominating-file default-directory f))
-            *project-identifiers*)))
-    (let ((tags-file (expand-file-name (format "~/.emacs.d/.tag-store/%s.tags"
-                 (file-name-nondirectory (directory-file-name project-root))))))
-      (ignore-errors (make-directory (file-name-directory tags-file) t))
-      (make-process
-       :name "ctags"
-       :buffer (get-buffer-create "*ctags*")
-       :command `("ctags" "--output-format=etags" "-f" ,tags-file "-R"
-      "--languages=all" "--exclude=node_modules" ,project-root)
-       :sentinel (lambda (_ event)
-       (when (string-match-p "finished" event)
-         (message "Tags generated at %s" tags-file))))
-      (when (file-exists-p tags-file) ;; assign newly generated tags file
-  (setq-local tags-table-list (list tags-file))
-  (visit-tags-table tags-file t)))))
-
-(defun +project-tags-load ()
-  "Load tags file for current project if it exists."
-  (when-let ((project-root (cl-some (lambda (f) (locate-dominating-file default-directory f))
-            *project-identifiers*)))
-    (let ((tags-file (expand-file-name (format "~/.emacs.d/.tag-store/%s.tags"
-                 (file-name-nondirectory (directory-file-name project-root))))))
-      (when (file-exists-p tags-file)
-  (setq-local tags-table-list (list tags-file))
-  (visit-tags-table tags-file t)))))
+  (+with-context
+   (let ((default-directory default-directory))
+     (make-process
+      :name "ctags"
+      :buffer nil
+      :command '("ctags" "-eR" "-f" ".tags" "--exclude=node_modules" ".")
+      :sentinel (lambda (_ e) (message "ctags: %s" (string-trim e)))))))
 
 (defun +flow-truncate-buffer-name ()
   "Show right 1/3 of buffer name, prepend '<' if truncated."
   (let* ((name (if buffer-file-name
-       (abbreviate-file-name buffer-file-name)
-     (buffer-name)))
-   (max-len (/ (window-width) 3)))
+                   (abbreviate-file-name buffer-file-name)
+                 (buffer-name)))
+         (max-len (/ (window-width) 3)))
     (if (<= (length name) max-len)
-  name
+        name
       (concat "<" (substring name (- (length name) (- max-len 1)))))))
 
 (defun +paredit-RET ()
@@ -186,21 +176,12 @@
      ('minibuffer-mode 'previous-matching-history-element)
      (_ 'paredit-raise-sexp))))
 
-(defun +time-current-tasks ()
-  "Clock in when entering CURRENT, clock out when leaving."
-  (when (and (bound-and-true-p org-state)
-             )
-    (if (string= org-state "CURRENT")
-        (org-clock-in)
-      (when (string= org-from-state "CURRENT")
-        (org-clock-out nil t)))))
-
 (defun +org-clock-todo-change ()
   (if (string= org-state "CURRENT")
       (org-clock-in)
     (org-clock-out-if-current)))
 
-;;; Input/Keybinds:
+;;; -Input/Keybinds:
 
 (dolist (bind
          `(
@@ -227,6 +208,7 @@
            ("C-c g" vc-dir-root)
            ("C-c C-g" vc-print-root-log)
            ("M-T" eshell)
+           ("M-R" +repl)
 
            ;; Editing
            ("M-/" replace-string)
@@ -260,13 +242,13 @@
   :custom
   (olivetti-style nil)
   (olivetti-body-width 80)
-  :hook (text-mode-hook . olivetti-mode))
+  :hook ((org-mode-hook
+          markdown-mode-hook)
+         . olivetti-mode))
 
 (use-package org :ensure nil
   :custom
   (org-ellipsis " ▼")
-  (org-hide-emphasis-markers t)
-  (org-export-with-section-numbers nil)
   (org-hide-emphasis-markers t)
   (org-export-with-section-numbers nil)
   (org-todo-keywords
@@ -323,8 +305,21 @@
 (dolist (hook '(prog-mode-hook css-mode-hook))
   (add-hook hook (lambda ()
                    (display-line-numbers-mode 1) (column-number-mode 1)
-                   (auto-revert-mode 1)
                    (display-fill-column-indicator-mode 1) (hl-line-mode 1))))
+
+(defun +project-tags-generate ()
+  (interactive)
+  (+with-context
+   (let ((default-directory default-directory))
+     (make-process
+      :name "ctags"
+      :buffer nil
+      :command '("ctags" "-eR" "-f" ".tags" "--exclude=node_modules" ".")
+      :sentinel (lambda (_ e) (message "ctags: %s" (string-trim e)))))))
+
+(defun +project-tags-load ()
+  (when-let ((f (locate-dominating-file default-directory ".tags")))
+    (visit-tags-table (expand-file-name ".tags" f) t)))
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'find-file-hook #'+project-tags-load)
